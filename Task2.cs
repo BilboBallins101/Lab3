5d59e3c055134842030387150486f66e2368c5cd
InfiniumKnight <163657942+InfiniumKnight@users.noreply.github.com>
2025-01-24 10:32:50 -0500

    Update Task2.cs
    the algorithm works and displays to the console correctly

--- Task2.cs
+++ Task2.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Task2 : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private double price; //price the customer pays
+    [SerializeField] private double numCopies; //number of copies the customer bought
+    [SerializeField] private double profit; //what the bookstore makes before costs
+    [SerializeField] private double costs; // what the bookstore pays to process the order
+
     void Start()
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
+        profit = price * numCopies; //profit would simply be price times how many copies were sold
+        Debug.Log("The profit before costs is " + profit);
+
+        costs = ((price*.6)*numCopies) + 3 + (.75*(numCopies-1)); //multiplying the price by .6 gets us what the bookstore pays for the book at 40% off.
+        Debug.Log("Costs the bookstore pays to process the purchase is " + costs);
         
     }
 }
