05e2e5d9571cfeca1dc10c96389a44f347dad849
BilboBallins101 <da742117@ucf.edu>
2025-01-23 22:48:35 -0500

    Updated task 1

--- Task1.cs
+++ Task1.cs
@@ -5,17 +5,44 @@ using UnityEngine;
 public class Task1 : MonoBehaviour
 {
 
-    public int myText = 0;
+    public int modules; //variables for the amount of work in a class
+    public int quizzes;
+    public int reading;
+    public int assignments;
+    public int timesTaught;
 
-    // Start is called before the first frame update
+    private float modulesScore; //variables for determining percentages of difficulty
+    private float quizzesScore;
+    private float readingScore;
+    private float assignmentsScore;
+    private float timesTaughtScore;
+
+    public string className; //string for putting name of the course
+    
+    [SerializeField] private float classDifficulty; //class difficulty score variable
+
+
     void Start()
     {
+       if (modules > 5) //sets maximum value on variables
+            modules = 5;
+       if (quizzes > 6)
+            quizzes = 6;
+       if (reading > 5)
+            reading = 5;
+       if(assignments > 6)
+            assignments = 6;
+       if (timesTaught > 5)
+            timesTaught = 5;
         
-    }
+        modulesScore = modules * 3; //calculates percentages
+        quizzesScore = quizzes * 5;
+        readingScore = reading * 3;
+        assignmentsScore = assignments * 5;
+        timesTaughtScore = timesTaught * 2;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        classDifficulty = (modulesScore + quizzesScore + readingScore + assignmentsScore + timesTaughtScore)/10; //determines class difficulty score
+
+        Debug.Log(className + " Class Difficulty = " +  classDifficulty); //displays class name and difficulty score in console
     }
 }
